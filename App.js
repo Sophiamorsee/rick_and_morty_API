8b6f9d5f144dfff631f6e6778c59e223cc351140
Sophiamorsee <103909194+Sophiamorsee@users.noreply.github.com>
2023-01-17 15:05:08 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Cards from "./components/Cards/Cards";
+import Filters from "./components/Filters/Filters";
+import Pagination from "./components/Pagination/Pagination";
+ 
+function App() {
+
+  let [pageNumber,setPageNumber] =useState (1);
+  let [fetchedData, updateFetchedData]=useState([ ]);
+  let {info, results} = fetchedData
+
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber }`; 
+
+  useEffect(()=>{  
+    (async function(){
+      let data = await fetch(api).then(res=>res.json())
+      updateFetchedData(data);
+    })();
+  },[api])
+ 
+
+  return (
+    <div className="App ">  
+    
+    <h1 className="text-center ubuntu my-4"> 
+     Sophia's Rick & Morty <span className ="text-info">Wiki</span>
+    
+    </h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-2"> 
+       {/* <Filters/>  */}
+        </div>
+        <div className="col-8"> 
+        <div className="row">
+        <Cards results= {results}/>
+        </div>
+        </div>
+        </div>
+        </div>
+
+      <Pagination setPageNumber ={setPageNumber}/>  
+     </div>
+    
+  );
+}
+
+export default App;
